d26a110b902eee254a9ef89738f5b29bb4c2975a
eduardodangelo <eduardo@siteassist.co>
2022-11-19 10:54:59 +0000

    add tabs to project page

--- src/components/ProjectPage.jsx
+++ src/components/ProjectPage.jsx
@@ -1,8 +1,36 @@
 import React from 'react'
 import PageLayout from './common/PageLayout'
+import { Box, Tab, Tabs, Typography } from '@mui/material'
+import TabContext from '@mui/lab/TabContext'
+import TabList from '@mui/lab/TabList'
+import TabPanel from '@mui/lab/TabPanel'
 
 const ProjectPage = () => {
-  return <PageLayout title='Project'>Hello world</PageLayout>
+  const [value, setValue] = React.useState('1')
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue)
+  }
+
+  return (
+    <PageLayout title='Projects / 2023 Year Goals'>
+      <Typography variant='h4'>2023 Year Goals</Typography>
+      <Box sx={{ width: '100%', typography: 'body1' }}>
+        <TabContext value={value}>
+          <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+            <TabList onChange={handleChange} aria-label='lab API tabs example'>
+              <Tab label='Overview' value='1' />
+              <Tab label='Stats' value='2' />
+              <Tab label='Config' value='3' />
+            </TabList>
+          </Box>
+          <TabPanel value='1'>Item One</TabPanel>
+          <TabPanel value='2'>Item Two</TabPanel>
+          <TabPanel value='3'>Item Three</TabPanel>
+        </TabContext>
+      </Box>
+    </PageLayout>
+  )
 }
 
 export default ProjectPage
