cbad618fd00b56be4c802bca9441b6612eab46df
eduardodangelo <eduardo@siteassist.co>
2022-11-22 17:23:54 +0000

    edit expand button

--- src/app/features/Okr.jsx
+++ src/app/features/Okr.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { Box, Card, Checkbox, Chip, Collapse, Divider, Typography } from '@mui/material'
+import { Box, Card, Checkbox, Chip, Collapse, Divider, IconButton, Typography } from '@mui/material'
 import RadioButtonUncheckedIcon from '@mui/icons-material/RadioButtonUnchecked'
 import CheckCircleIcon from '@mui/icons-material/CheckCircle'
 import ProgressBar from '../common/ProgressBar'
@@ -7,6 +7,11 @@ import Description from './Description'
 import TrashButton from '../common/TrashButton'
 import OkrTitle from './OKRTitle'
 import StatusSelect from '../common/StatusSelect'
+import UnfoldMoreIcon from '@mui/icons-material/UnfoldMore'
+import UnfoldMoreDoubleIcon from '@mui/icons-material/UnfoldMoreDouble'
+import ExpandIcon from '@mui/icons-material/Expand'
+import MoreHorizIcon from '@mui/icons-material/MoreHoriz'
+import KeyboardArrowDownSharpIcon from '@mui/icons-material/KeyboardArrowDownSharp'
 
 const Okr = ({ title, description, onDelete, onChange = () => {} }) => {
   const [expanded, setExpanded] = useState(false)
@@ -27,13 +32,22 @@ const Okr = ({ title, description, onDelete, onChange = () => {} }) => {
           icon={<RadioButtonUncheckedIcon />}
           checkedIcon={<CheckCircleIcon />}
         />
-        <Box flex={2} sx={{ cursor: 'pointer' }} onClick={() => setExpanded(!expanded)}>
+        <Box flex='auto' sx={{ cursor: 'pointer' }}>
           <OkrTitle value={title} onUpdate={onChange} />
         </Box>
+        <Box flex={1} />
         <Box flex={1}>
           <ProgressBar value={40} />
         </Box>
         <StatusSelect />
+        <IconButton onClick={() => setExpanded(!expanded)} size='small' sx={{ ml: 1 }}>
+          <KeyboardArrowDownSharpIcon
+            sx={{
+              transform: expanded ? 'rotate(-180deg)' : 'rotate(0)',
+              transition: '0.2s',
+            }}
+          />
+        </IconButton>
         <TrashButton className='hidden-button' onDelete={onDelete} sx={{ ml: 1 }} />
       </Box>
       <Collapse in={expanded}>
